3e01dab696693df94519dd2a0e8d2dfd91c5b220
Grazia Palombella <grazia.palombella@gmail.com>
2022-03-15 16:04:42 +0100

    init project

---
+++ dist/Game.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Game;
+
+var _reactNative = require("react-native");
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Game() {
+  return /*#__PURE__*/_react.default.createElement(_reactNative.View, null, /*#__PURE__*/_react.default.createElement(_reactNative.Text, null, "Game"));
+}
+
+var styles = _reactNative.StyleSheet.create({});
---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Game", {
+  enumerable: true,
+  get: function get() {
+    return _Game.default;
+  }
+});
+
+var _Game = _interopRequireDefault(require("./Game"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
