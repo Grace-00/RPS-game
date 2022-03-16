fb3b1ad4e66b6355623533e97475949f9ac6e248
Grazia Palombella <grazia.palombella@gmail.com>
2022-03-16 09:47:18 +0100

    package version changed

--- dist/Game.js
+++ dist/Game.js
@@ -12,7 +12,7 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function Game() {
-  return /*#__PURE__*/_react.default.createElement(_reactNative.View, null, /*#__PURE__*/_react.default.createElement(_reactNative.Text, null, "Game"));
+  return /*#__PURE__*/_react.default.createElement(_reactNative.View, null, /*#__PURE__*/_react.default.createElement(_reactNative.Text, null, "Game"), /*#__PURE__*/_react.default.createElement(_reactNative.Text, null, "Text"));
 }
 
 var styles = _reactNative.StyleSheet.create({});
